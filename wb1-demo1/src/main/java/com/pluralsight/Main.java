f45bba89b92fae2fddce620e1c04cdbebdf64c50
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-14 11:52:10 -0400

    Finish first lecture

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -14,8 +14,34 @@ public class Main {
 
         greeting = word1 + " " + word2 + "!";
 
-
         System.out.println (greeting);
+        ///////////////////////////////////////////////////////////////////
+
+        float taxRate = 0.15f;
+
+        long aLargeNumber = 2222222L;
+
+        ////////////////////////////////////////////////////////////////////
+
+        int aint;
+
+        long blong;
+
+        aint = 7;
+
+        blong = aint;
+        //////////////////////////////////////////////////////////////////////
+
+        long aLong;
+        int bInt;
+
+        aLong = 7777777777_000_000_000L;
+        bInt = (int) aLong;
+
+
+        System.out.println(bInt);
+
+
 
     }
 

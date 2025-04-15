d633e1dec212fe4fa1eb332949e906bdcb81b28b
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-15 09:16:00 -0400

    Payroll Calculator with pay rate increase

--- BasicCalculator/src/main/java/com/pluralsight/Calculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -18,10 +18,41 @@ public class Calculator {
         String letter;
         letter = scanner.nextLine();
 
-        System.out.println("You've chosen: " + letter);
-
+        //System.out.println("You've chosen: " + letter);
         double product = number1 * number2;
-        System.out.printf("The product of %.1f and %.1f is %.1f\n", number1, number2, product);
+        double sum = number1 + number2;
+        double difference = number1 - number2;
+        double remainder = number1 / number2;
+
+
+        if (letter.equals("a")) {
+
+            System.out.println("You've chosen addition");
+            System.out.printf("The sum of %.1f and %.1f is %.1f\n", number1, number2, sum);
+
+        } else if (letter.equals("s")) {
+
+            System.out.println("You've chosen subtraction");
+            System.out.printf("The difference between %.1f and %.1f is %.1f\n", number1, number2, difference);
+        } else if ( letter.equals("m")) {
+
+            System.out.println("You've chosen multiplication");
+            System.out.printf("The product of %.1f and %.1f is %.1f\n", number1, number2, product);
+
+        } else if ( letter.equals("d")) {
+
+            System.out.println("You've chosen division");
+            System.out.printf("The remainder of %.1f and %.1f is %.1f\n", number1, number2, remainder);
+        }
+
+
+//
+//        double product = number1 * number2;
+//        double sum = number1 + number2;
+//        double difference = number1 - number2;
+//        double remainder = number1 / number2;
+//
+//        System.out.printf("The product of %.1f and %.1f is %.1f\n", number1, number2, product);
 
 
 

d2e338bdbc3f003eddba79f76ea3e90ba3b8a57a
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-15 15:14:56 -0400

    PayRoll Modified

--- PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
@@ -3,30 +3,60 @@ import java.util.Scanner;
 
 public class Payroll {
 
-    public static void main(String[] args){
+
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        System.out.println("Okay "+ name + ". Now what was your hours worked?");
-        double hours_worked = scanner.nextDouble();
-        System.out.println("Finally what is your pay rate?");
-        double pay_rate = scanner.nextDouble();
-        if (hours_worked > 40) {
-            pay_rate = pay_rate * 1.5;
-
-        }
-        double gross_pay = hours_worked * pay_rate;
-
-        System.out.println("Hey " + name + ". Here is your pay " + gross_pay );
-        System.out.printf("%s worked %.1f hours at an hourly rate of $%.1f and made $%.1f", name, hours_worked, pay_rate, gross_pay);
-        System.out.println(hours_worked * pay_rate);
-
-
-
-
-
+        String name = promptforName();
+        float hours_worked = promptforHoursWorked();
+        double pay_rate = promptForPayRate();
+        double gross_pay = calcGrossPay(hours_worked,pay_rate);
+        System.out.printf("%s  has worked for %.2f hours at a rate of $%.2f and made $%.2f\n", name, hours_worked, pay_rate, gross_pay);
 
 
 
     }
+
+    /**
+     * Calculates Gross Pay, considers overtime over 40 hours
+     */
+    public static double calcGrossPay(float hours_Worked, double pay_rate) {
+        if (hours_Worked <= 40){
+            return  hours_Worked * pay_rate;
+        }
+        else {
+            float overtimeHours = hours_Worked - 40;
+            double standard_pay = 40 * pay_rate;
+            double overtimeRate = pay_rate * 1.5;
+            double overtimePay = overtimeHours * overtimeRate;
+
+            return  standard_pay + overtimePay;
+
+
+        }
+
+
+    }
+
+    public static String promptforName(){
+        System.out.println("Please enter the employee name");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public  static float promptforHoursWorked(){
+        System.out.println("Please Enter The Number Of Hours You Worked This Pay Period: ");
+        Scanner scanner = new Scanner(System.in);
+        return  scanner.nextFloat();
+    }
+
+    public static double promptForPayRate(){
+        System.out.println("Please Enter In Your Base Pay Rate: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+
+
+    }
+
 }

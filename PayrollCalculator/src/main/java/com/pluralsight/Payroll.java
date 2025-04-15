e0c8cc7796d35983f42cb4bde722a0d3892115ea
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-15 09:17:52 -0400

    Basic Calculator completed

--- PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Payroll.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Payroll {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Okay "+ name + ". Now what was your hours worked?");
+        double hours_worked = scanner.nextDouble();
+        System.out.println("Finally what is your pay rate?");
+        double pay_rate = scanner.nextDouble();
+        if (hours_worked > 40) {
+            pay_rate = pay_rate * 1.5;
+
+        }
+        System.out.println("Hey " + name + ". Here is your pay");
+        System.out.println(hours_worked * pay_rate);
+
+
+
+
+
+
+
+
+    }
 }

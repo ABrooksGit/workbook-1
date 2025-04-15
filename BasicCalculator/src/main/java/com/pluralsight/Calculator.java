1d9921da4bb1f2107fa693a6a8d54350ae8a4db5
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-15 10:23:00 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

--- BasicCalculator/src/main/java/com/pluralsight/Calculator.java
+++ BasicCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -12,12 +12,16 @@ public class Calculator {
         double number1 = scanner.nextDouble();
         System.out.println("What is the Second number");
         double number2 =scanner.nextDouble();
-
-        System.out.println("Please choose between (A) Addition, (S) Subtraction, (M) Multiplication or (D) Division");
         scanner.nextLine();
+
+        System.out.println("Possible calculations:");
+        System.out.println("(A) Addition");
+        System.out.println("(S) Subtraction");
+        System.out.println("(M) Multiplication ");
+        System.out.println("(D) Division");
+        System.out.println("Please select an option: ");
         String letter;
         letter = scanner.nextLine();
-
         //System.out.println("You've chosen: " + letter);
         double product = number1 * number2;
         double sum = number1 + number2;
@@ -29,20 +33,23 @@ public class Calculator {
 
             System.out.println("You've chosen addition");
             System.out.printf("The sum of %.1f and %.1f is %.1f\n", number1, number2, sum);
+            System.out.printf("%.1f + %.1f = %.1f\n", number1, number2, sum);
 
         } else if (letter.equals("s")) {
 
             System.out.println("You've chosen subtraction");
             System.out.printf("The difference between %.1f and %.1f is %.1f\n", number1, number2, difference);
+            System.out.printf("%.1f - %.1f = %.1f\n", number1, number2, difference);
         } else if ( letter.equals("m")) {
 
             System.out.println("You've chosen multiplication");
             System.out.printf("The product of %.1f and %.1f is %.1f\n", number1, number2, product);
-
+            System.out.printf("%.1f * %.1f = %.1f\n", number1, number2, product);
         } else if ( letter.equals("d")) {
 
             System.out.println("You've chosen division");
             System.out.printf("The remainder of %.1f and %.1f is %.1f\n", number1, number2, remainder);
+            System.out.printf("%.1f / %.1f = %.1f\n", number1, number2, remainder);
         }
 
 

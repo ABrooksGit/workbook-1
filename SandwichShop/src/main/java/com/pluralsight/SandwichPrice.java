a5702e83a02b4bb5c0580a22c25ef5caaad58459
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-16 09:20:17 -0400

    SandwichShop Part 2

--- SandwichShop/src/main/java/com/pluralsight/SandwichPrice.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichPrice.java
@@ -32,18 +32,62 @@ public class SandwichPrice {
         System.out.println("Please Select Your Sandwich Size Of Choice: ");
         String numbers = scanner.nextLine();
 
-        double selectedSandiwch = 0;
+
+        double selectedSandwich = 0;
 
         if(numbers.equals("1")){
             System.out.println("You have chosen Regular Size for $5.45");
-            selectedSandiwch = 5.45;
+            selectedSandwich = 5.45;
+            System.out.println("Would you like your sandwich to be loaded for an extra dollar");
+            System.out.println("yes or no");
+            String loaded = scanner.nextLine();
+            if (loaded.equals("yes")){
+                System.out.println("A loaded regular sandwich is coming right up");
+                selectedSandwich = 6.45;
+            }else{
+                System.out.println("Understood, you will not have a loaded regular sandwich");
+            }
         } else if(numbers.equals("2")){
             System.out.println("You have chosen the Large Size for $8.95");
-            selectedSandiwch = 8.95;
+            selectedSandwich = 8.95;
+            System.out.println("Would you like your sandwich to be loaded for an extra $1.75");
+            System.out.println("yes or mo");
+            String loaded = scanner.nextLine();
+            if(loaded.equals("yes")){
+                System.out.println("A loaded large sandwich is coming right up");
+                selectedSandwich = 10.70;
+            } else{
+                System.out.println("Understood, you will not be having a loaded large sandwich");
+            }
+
         }
 
 
-        return selectedSandiwch;
+//        if(numbers.equals("1")){
+//            System.out.println("Would you like your sandwich to be loaded for an extra dollar");
+//            System.out.println("Yes or No");
+//            if(loaded.equals("Yes")){
+//                System.out.println("A loaded regular sandwich is coming right up");
+//                selectedSandwich = 6.45;
+//            }else {
+//                System.out.println("Understood, you will not have a loaded regular sandwich");
+//
+//            }
+//        } else if (numbers.equals("2")) {
+//            System.out.println("Would you like your sandwich to be loaded for an extra $1.75");
+//            System.out.println("Yes or No");
+//            if (loaded.equals("Yes")){
+//                System.out.println("A loaded large sandwich is coming right up");
+//                selectedSandwich = 10.70;
+//            } else {
+//                System.out.println("Understood, you will not have a loaded large sandwich");
+//
+//            }
+//
+//        }
+
+
+        return selectedSandwich;
 
 
 
